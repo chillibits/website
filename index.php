a8ff25ea9972d43d87fdaa181368e1b1f6f6b49a
Marc Auberer <marc.auberer@chillibits.com>
2020-01-12 12:13:29 +0100

    Add logging to forwarding script

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0,2);
 
+    // write to logfile
+	$date = str_replace(".", "_", date('Y.m.d'));
+	createDirIfNotExists("./logs/logfiles_".str_replace(".", "_", date('Y.m')));
+	$logfile = fopen("./logs/logfiles_".str_replace(".", "_", date('Y.m'))."/logfile_$date.txt", "a");
+	$date = date('H:i:s');
+	fwrite($logfile, "$date - $lang\n");
+	fclose($logfile);
+
     if($lang == "de") {
         header("Location: ./de/");
     } else {
@@ -8,4 +16,11 @@
         header("Location: ./en/");
     }
     exit;
+
+    //--------------------------------------------------------------------Funktionen--------------------------------------------------------------------
+
+	function createDirIfNotExists($path) {
+		if(!file_exists($path)) return mkdir($path, 0777, true);
+		return false;
+	}
 ?>
